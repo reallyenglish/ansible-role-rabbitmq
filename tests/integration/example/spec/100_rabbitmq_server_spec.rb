aae1c2e856cbd964c266c3c8bcb62006217e5833
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-06-15 10:54:22 +0900

    QA

--- tests/integration/example/spec/100_rabbitmq_server_spec.rb
+++ tests/integration/example/spec/100_rabbitmq_server_spec.rb
@@ -1,14 +1,14 @@
 require "spec_helper"
 
 context "after provision finishes" do
-  [ server(:server1), server(:server2) ].each do |s|
+  [server(:server1), server(:server2)].each do |s|
     describe s do
-      [
-        "rabbitmq_management",
-        "rabbitmq_delayed_message_exchange"
-      ].each do |p|
-      it "has #{p} plug-ins installed" do
-        r = current_server.ssh_exec("sudo rabbitmq-plugins list")
+      %w(
+        rabbitmq_management
+        rabbitmq_delayed_message_exchange
+      ).each do |p|
+        it "has #{p} plug-ins installed" do
+          r = current_server.ssh_exec("sudo rabbitmq-plugins list")
           expect(r).to match(/\s#{p}\s/)
         end
       end
