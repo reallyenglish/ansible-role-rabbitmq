94c51fbbf91ef7424fd46a5dd98b1bf206fdd72c
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-06-23 17:53:51 +0900

    increase # of nodes, test all ports are open

--- tests/integration/example/spec/000_default_spec.rb
+++ tests/integration/example/spec/000_default_spec.rb
@@ -34,4 +34,35 @@ context "after provisioning finished" do
       expect(r).to match(/^1 packets transmitted, 1 packets received, 0.0% packet loss$/)
     end
   end
+
+  # https://www.rabbitmq.com/networking.html
+  ports = [
+    4369, # epmd
+    5672, # AMQP
+    5671, # AMQP TLS
+    25672 # Erlang
+  ]
+  servers =[
+    :server1,
+    :server2,
+    :server3
+  ]
+  servers.each do |s|
+    describe server(s) do
+      others = servers.reject{|i| i == s }
+      others.each do |dest|
+        describe firewall(server(dest)) do
+
+          # IP address is used in this test
+          it { is_expected.to be_reachable }
+          ports.each do |p|
+            it do
+              pending "be_reachable does not work on FreeBSD https://github.com/otahi/infrataster-plugin-firewall/pull/4"
+              is_expected.to be_reachable.tcp.dest_port(p)
+            end
+          end
+        end
+      end
+    end
+  end
 end
