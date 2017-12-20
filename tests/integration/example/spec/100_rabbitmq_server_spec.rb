d4c91a6032db308a1b97a0bc760241f0eafe2a8c
Mitsuru Y <mitsuruy@reallyenglish.com>
2017-12-20 13:45:39 +0900

    [bugfix] fix thirdparty repo, bump rubocop and QA

--- tests/integration/example/spec/100_rabbitmq_server_spec.rb
+++ tests/integration/example/spec/100_rabbitmq_server_spec.rb
@@ -3,10 +3,10 @@ require "spec_helper"
 context "after provision finishes" do
   [server(:server1), server(:server2), server(:server3)].each do |s|
     describe s do
-      %w(
+      %w[
         rabbitmq_management
         rabbitmq_delayed_message_exchange
-      ).each do |p|
+      ].each do |p|
         it "has #{p} plug-ins installed" do
           r = current_server.ssh_exec("sudo rabbitmq-plugins list -E -m")
           expect(r).to match(/^#{p}$/)
